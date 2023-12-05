1b37bc8b24c4144fe4081738030f0aea0ae73bb6
Amira El-Garf <elgarfamira4@gmail.com>
2023-12-05 22:01:46 +0200

    inset a pet + user signin and regester is done and tested

--- server/index.js
+++ server/index.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
+var tempname = "";
+
+
+//chatgpt
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
 // MySQL Connection
-const db = mysql.createConnection({
-  host: 'localhost',
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
   user: 'root',
   password: 'PurrfectMatch',
   database: 'PurrfectMatch',
 });
 
 // Connect to MySQL
-db.connect((err) => {
+connection.connect((err) => {
   if (err) {
     console.error('MySQL connection error: ' + err.stack);
     return;
@@ -21,7 +33,218 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-// Your Express routes and logic go here
+//user login
+app.post("/user/login", (req, res) => {
+  const { username, password } = req.body;
+  connection.query(
+    "select * from user where UserID = ? and FirstName = ?",
+    [username, password],
+    function (err, row) {
+      if (err) {
+        return res.status(500).json();
+      } else if (!row.length) {
+        return res.status(404).json();
+      }
+      return res.status(201).json(row);
+    }
+  );
+});
+
+//user register
+app.post("/user/register", (req, res) => {
+  const { username, password, type } = req.body;
+  connection.query(
+    "insert into authentication values (?,?,?)",
+    [username, password, type],
+    function (err, row) {
+      if (err) {
+        return res.status(500).json();
+      }
+      //removed the second if cause register doesnt return anything to check on if length < 0
+      tempname = username;
+      return res.status(201).json(row);
+    }
+  );
+});
+
+//user register
+app.post("/user/customer-data", (req, res) => {
+  console.log(tempname);
+  const {
+    id,
+    fname,
+    lname,
+    date,
+    age,
+    gender,
+    energy,
+    mind,
+    nature,
+    identity,
+    tactics,
+  } = req.body;
+
+  connection.query(
+    "insert into user values (?,?,?,?,?,?)",
+    [id, fname, lname, date, age, gender],
+    function (err, row1) {
+      if (err) {
+        return res.status(500).json();
+      } else {
+        if (tempname === "") {
+          return res.status(500).json();
+        }
+        connection.query(
+          "insert into customer values (?,?,?,?,?,?,?)",
+          [id, energy, mind, nature, identity, tactics, tempname],
+          function (err, row2) {
+            if (err) {
+              return res.status(500).json();
+            }
+          }
+        );
+        return res.status(201).json(row1);
+      }
+    }
+  );
+});
+
+//list of all pets 
+app.get("/pets", (req, res) => {
+  connection.query("SELECT * FROM pet", function (err, rows) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    return res.status(200).json(rows);
+  });
+});
+
+//get details of a specific pet
+app.get("/pets/:id", (req, res) => {
+  const petId = req.params.id;
+  connection.query("SELECT * FROM pet WHERE PetID = ?", [petId], function (err, row) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    } else if (!row.length) {
+      return res.status(404).json({ error: "Pet not found" });
+    }
+    return res.status(200).json(row);
+  });
+});
+
+
+//update pet details 
+//7ewar el type id hal manteky keda?
+app.put("/pets/:id", (req, res) => {
+  const petId = req.params.id;
+  const { name, gender, breed, typeName, dateOfBirth, centerId, userId } = req.body;
+
+  connection.query(
+    "SELECT TypeID FROM type WHERE TypeName = ?",
+    [typeName],
+    function (err, rows) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      } else if (rows.length === 0) {
+        return res.status(404).json({ error: "Type not found" });
+      }
+
+      const typeId = rows[0].TypeID;
+
+      connection.query(
+        "UPDATE pet SET Name = ?, Gender = ?, Breed = ?, TypeID = ?, DateOfBirth = ?, CenterID = ?, UserID = ? WHERE PetID = ?",
+        [name, gender, breed, typeId, dateOfBirth, centerId, userId, petId],
+        function (err, result) {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal Server Error" });
+          } else if (result.affectedRows === 0) {
+            return res.status(404).json({ error: "Pet not found" });
+          }
+          return res.status(200).json({ message: "Pet updated successfully" });
+        }
+      );
+    }
+  );
+});
+
+
+
+//Delete a certain pet 
+
+app.delete("/pets/:id", (req, res) => {
+  const petId = req.params.id;
+  connection.query("DELETE FROM pet WHERE PetID = ?", [petId], function (err, result) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    } else if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Pet not found" });
+    }
+    return res.status(200).json({ message: "Pet deleted successfully" });
+  });
+});
+
+
+//insert a new pet
+app.post("/pets/insert", (req, res) => {
+  const { PetID, name, gender, breed, type, dateOfBirth, userId, centerName } = req.body || {};
+
+  console.log(req.body);
+
+  // First, query the TypeID based on the provided Type
+  connection.query(
+    "SELECT TypeID FROM Type WHERE TypeName = ?",
+    [type],
+    function (err, rows) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      } else if (rows.length === 0) {
+        return res.status(404).json({ error: "Type not found" });
+      }
+
+      const typeId = rows[0].TypeID;
+
+      // Now, query the CenterID based on the provided CenterName
+      connection.query(
+        "SELECT CenterID FROM Center WHERE CenterName = ?",
+        [centerName],
+        function (err, rows) {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal Server Error" });
+          } else if (rows.length === 0) {
+            return res.status(404).json({ error: "Center not found" });
+          }
+
+          const centerId = rows[0].CenterID;
+
+          // Now, insert the pet with the obtained TypeID and CenterID
+          connection.query(
+            "INSERT INTO pet (PetID, Name, Gender, Breed, TypeID, DateOfBirth, UserID, CenterID) VALUES (?,?, ?, ?, ?, ?, ?, ?)",
+            [PetID, name, gender, breed, typeId, dateOfBirth, userId, centerId],
+            function (err, result) {
+              if (err) {
+                console.error(err);
+                return res.status(500).json({ error: "Internal Server Error" });
+              }
+
+              return res.status(201).json({ petId: result.insertId });
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

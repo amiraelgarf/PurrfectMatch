cfcd8d113bc1cd190bcdfb83696d450adeee3075
danahhassan <danah.hassan@hotmail.com>
2023-12-12 23:10:19 +0200

    updated profile too fel navbar

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,10 +2,16 @@ import NavBar from "./NavBar";
 import './Profile.css';
 
 export default function Profile(){
+    const navStyle = {
+        borderColor: '#6F3BB2',
+        borderStyle: 'solid',
+        borderWidth: '5px'
+    };
+
     return(
         <div className = "background3">
         <h1>Profile</h1>
-        <NavBar/>
+        <NavBar style={navStyle}/>
         </div>
     )
 }

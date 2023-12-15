7acf8bb4be1c49ede4ab7c81e25ed47ed1be6490
Amira El-Garf <elgarfamira4@gmail.com>
2023-12-16 01:13:38 +0200

    pets done

--- client/src/pages/Pets.jsx
+++ client/src/pages/Pets.jsx
@@ -1,75 +1,126 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import NavBar from "./NavBarCustomer";
 import Layout from "./Layout";
-import './Pets.css'
-
+import "./Pets.css";
 
 function ImageBackground(style) {
-  fstyle = { cStandard, style }
-  return (
-    < div style={fstyle} ></div >
-  );
+  fstyle = { cStandard, style };
+  return <div style={fstyle}></div>;
 }
 
 export default function Pets() {
-
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-
-        const response = await axios.get('http://localhost:3000/pets');
+        const response = await axios.get("http://localhost:3000/pets");
         console.log(response);
-        setData(response.data)
+        setData(response.data);
       } catch (error) {
         // Step 6: Handle errors and update the error state
         console.log(error);
       }
+    };
+    const fetchBreed = async () => {
+      try {
+        //todo get breed by the id
+        const response = await axios.get("http://localhost:3000/pets");
+        console.log(response);
+        setData(response.data);
+      } catch (error) {
+        // Step 6: Handle errors and update the error state
+        console.log(error);
       }
-      fetchData();
+    };
+
+    fetchData();
   }, []);
+
 
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate('/mycart');
+    navigate("/mycart");
   };
 
-  const gotoPet = () => {
-    navigate('/viewpet');
-  }
-  function PetsImages() {
 
+  const gotoPet = (PetID) => {
+    navigate("/viewpet", {state: {petid:PetID}});
+  };
+  function PetsImages() {
     return (
       <div>
-        <img src="./Images/search.png" alt="Description" className="searchicon" />
+        <img
+          src="./Images/search.png"
+          alt="Description"
+          className="searchicon"
+        />
         <img src="./Images/search.png" alt="Description" className="arrow" />
       </div>
     );
   }
   return (
-    <div className="background2">
-      <div className="search">Search</div>
+    <div className="background2-pet-page-container">
+      <div className="background2-pet-page">
+        <div className="side-bar-container">
+          <div className="search-bar-side">
+            <img
+              src="./Images/search.png"
+              alt="Description"
+              className="searchIconforSideBar"
+            />
+            <div className="searchbar-side-bar3">Search</div>
+          </div>
+          <ul className="filter-list-side">
+            <li>
+              <img src="./Images/arrow.png" alt="Description" />
+              Types
+            </li>
+            <li>
+              <img src="./Images/arrow.png" alt="Description" />
+              Brand
+            </li>
+            <li>
+              <img src="./Images/arrow.png" alt="Description" />
+              Weight
+            </li>
+            <li>
+              <img src="./Images/arrow.png" alt="Description" />
+              Availablity
+            </li>
+            <li>
+              <img src="./Images/arrow.png" alt="Description" />
+              Category
+            </li>
+          </ul>
+        </div>
+        <div className="items-container">
+          {data?.map((pet) => (
+            <div className="item-item" key= {pet.PetID} >
+              <div className="item-inside-image">
+                <img src="./Images/Sunshine.png" alt="Description" />
+              </div>
+              <div className="item-label-name">{pet.Name}</div>
+              {/* <div> {pet.Breed} {types[pet.typeID]}</div> */}
+              <div> {pet.Breed} </div>
+              <button onClick={()=>gotoPet(pet.PetID)} className="more-info-btn">
+                More Info
+              </button>
+            </div>
+          ))}
+        </div>
+
+        {/*<div className="search">Search</div>
       <div className="searchbar"></div>
-      <div className="filter" ></div>
+      <div className="filter"></div>
 
-      {
-        data?.map((pet)=> (
-          <>
-        <img src="./Images/Sunshine.png" alt="Description" className="Sunshine" />
-        <div ></div>
-        <div >{pet.Name}</div>
-        <div >{pet.Breed}</div>
-        <button onClick={gotoPet} className='bSunshine'>More Info</button>
-        </>
-        ))
-      }
+      {/**/}
 
-      {/* <img src="./Images/Sunshine.png" alt="Description" className="Sunshine" />
+        {/* <img src="./Images/Sunshine.png" alt="Description" className="Sunshine" />
       <div className='cSunshine'></div>
       <div className='tSunshine'>Sunshine</div>
       <div className='dSunshine'>Cat, Persion</div>
@@ -87,11 +138,11 @@ export default function Pets() {
       <div className='dPeanut'>Dog, German Pinscher</div>
       <button onClick={gotoPet} className='bPeanut'>More Info</button> */}
 
-      <div className='c1'></div>
-      <div className='c2'></div>
-      <div className='c3'></div>
+        {/*<div className="c1"></div>
+      <div className="c2"></div>
+      <div className="c3"></div>*/}
 
-      <img src="./Images/search.png" alt="Description" className="searchicon" />
+        {/*<img src="./Images/search.png" alt="Description" className="searchicon" />
 
       <button onClick={handleClick} className="mycartpurple2">
         <img src="./Images/cart PURPLE.png" alt="Description" className="mycartpurple2-icon" />
@@ -108,9 +159,11 @@ export default function Pets() {
 
       <img src="./Images/arrow.png" alt="Description" className="amatches" />
       <button className="smatches">Personality Matches</button>
-      <NavBar />
-      <Layout />
-
+      
+    */}
+        <NavBar />
+        <Layout />
+      </div>
     </div>
   );
 }
--- client/src/pages/ViewPet.jsx
+++ client/src/pages/ViewPet.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 import NavBar from './NavBarCustomer';
 import './ViewPet.css';
 import Layout from "./Layout";
 
 
 export default function ViewPet() {
+    const location = useLocation();
+    const [PetID, setPetID] = useState(null);
+    const [Petdata, setPetdata] = useState(null);
+
+    useEffect(() => {
+    setPetID(location?.state.petid);
+    console.log(location?.state);
+  }, [location.state]);
     const navigate = useNavigate();
     const handleClick = () => {
         navigate('/mycart');
@@ -16,6 +26,28 @@ export default function ViewPet() {
     const goback = () => {
         navigate('/pets');
     }
+    
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(`http://localhost:3000/pets/${PetID}`);
+            console.log(response);
+            setPetdata(response.data);
+          } catch (error) {
+            // Step 6: Handle errors and update the error state
+            console.log(error);
+          }
+        };
+        if(PetID!=null)
+        {
+            fetchData();
+        }
+        
+        },[PetID])
+
+        console.log(PetID);
+
+
     return (
         <div className="background5">
             <div className='rectangle'></div>
@@ -37,9 +69,9 @@ export default function ViewPet() {
                 <br />
                 Sunshine was bred in one of our adoption centers personally and has been super playful with our staff.
             </p>
-            <button onClick={products} className='b1'>Products</button>
-            <button onClick={goback} className='b2'>Go Back</button>
-            <button className='b3'>Adopt</button>
+            <button onClick={products} className='button1'>Products</button>
+            <button onClick={goback} className='button2'>Go Back</button>
+            <button className='button3'>Adopt</button>
 
             <div className='tSunshine2' id=''>Sunshine</div>
             <button onClick={handleClick} className="mycartpurple5">
